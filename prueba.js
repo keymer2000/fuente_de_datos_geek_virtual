fcd5a9a81dedfdc66ca151d4a34278d42a3b2bfb
Keymer Jimenez <keimeralexanderj@gmail.com>
2021-01-26 17:25:58 -0500

    se modifico el package.json

---
+++ prueba.js
@@ -0,0 +1,27 @@
+// {
+//     "name": "fuente_de_datos_geek_virtual",
+//     "version": "1.0.0",
+//     "description": "Esta aplicación es una api-fake que se desarrolla para poder extraer datos de un BackEnd y de esta manera lograr la implementación de los servicios que se exponen desde este componente y puedan ser consumidos a través de fetch en el FrontEnd",
+//     "main": "index.js",
+//     "scripts": {
+//       "start": "node index.js",
+//       "now:deploy": "now"
+//     },
+//     "repository": {
+//       "type": "git",
+//       "url": "git+https://github.com/keymer2000/fuente_de_datos_geek_virtual.git"
+//     },
+//     "keywords": [],
+//     "author": "",
+//     "license": "ISC",
+//     "bugs": {
+//       "url": "https://github.com/keymer2000/fuente_de_datos_geek_virtual/issues"
+//     },
+//     "homepage": "https://keymer2000.github.io/Plataforma-Geek-Virtual/",
+//     "dependencies": {
+//       "faker": "^5.1.0",
+//       "json-server": "^0.16.2",
+//       "vercel": "^20.1.2"
+//     }
+//   }
+  
